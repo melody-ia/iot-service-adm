469707b803e3352eb1d98d421894106013e1ac6a
yonghwan <coxldtpeo1@naver.com>
2023-06-13 14:53:31 +0900

    컴포넌트 구조 정리(회원정보)

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./assets/scss/App.css";
 // component
 import Login from "./routes/login/Login";
-import UserList from "./routes/user/UserList";
-import UserAdd from "./routes/user/UserAdd";
-import UserInfo from "./routes/user/UserInfo";
+import UserList from "./routes/user_mgmt/UserList";
+import UserAdd from "./routes/user_mgmt/UserAdd";
+import UserBasicInfo from "./routes/user_info/UserBasicInfo";
 import Templeate from "./routes/layout/Template";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Route path="/" element={<Templeate />}>
         <Route path="UserList" element={<UserList />} />
         <Route path="UserList/add" element={<UserAdd />} />
-        <Route path="UserInfo/:id" element={<UserInfo />} />
+        <Route path="UserBasicInfo/:id" element={<UserBasicInfo />} />
         <Route />
         <Route />
         <Route />
