e30e8bdd8fb442ed3e37c43d3620ce98e7d80e35
SeongKon <canoa950327@gmail.com>
2024-03-06 20:45:15 +0900

    [수정] 탄소 중립 랭킹 관리

--- src/routes/event/RankingSetting.jsx
+++ src/routes/event/RankingSetting.jsx
@@ -1,5 +1,13 @@
-import { Lnb, CurrentBox, Pagination } from "../../components/bundle_components";
-import { useSelectBox, useCheckToken, useDatePicker } from "../../hooks/bundle_hooks";
+import {
+  Lnb,
+  CurrentBox,
+  Pagination,
+} from "../../components/bundle_components";
+import {
+  useSelectBox,
+  useCheckToken,
+  useDatePicker,
+} from "../../hooks/bundle_hooks";
 import { useEffect, useState } from "react";
 
 export default function RankingSetting() {
@@ -12,18 +20,21 @@ export default function RankingSetting() {
   const loadHistory = async () => {
     const data = {
       mb_no,
-      start_at,
-      end_at,
+      start_at: start_at,
+      end_at: end_at,
     };
-    const res = await postData("point/ranking", data);
+    const res = await postData("member/ranking", data);
     setBeforeFilter({ ...data });
     setPageData(res.page);
     setCurPage(1);
     if (!res.data) setResData([]);
   };
 
-  const loadPageData = async page => {
-    const res = await postData("point/ranking", { ...beforeFilter, cur_page: page });
+  const loadPageData = async (page) => {
+    const res = await postData("member/ranking", {
+      ...beforeFilter,
+      cur_page: page,
+    });
     setPageData(res.page);
   };
 
@@ -35,14 +46,25 @@ export default function RankingSetting() {
     <>
       <Lnb lnbType="event" />
       {/* <CurrentBox mod={true} del={true} down={true} tit="탄소 중립 랭킹 관리" /> */}
-      <CurrentBox btns={[/* "down" */]} tit="탄소 중립 랭킹 관리" />
+      <CurrentBox
+        btns={
+          [
+            /* "down" */
+          ]
+        }
+        tit="탄소 중립 랭킹 관리"
+      />
       <div className="ranking_setting box_ty01 table_type table_comm">
         <div className="filter_wrap d-flex">
           <div className="date_input_wrap d-flex">
             <div className="date_input input_ty02">{date.start}</div>
             <div className="date_input input_ty02">{date.end}</div>
           </div>
-          <button type="button" className="btn_ty01 btn_search" onClick={loadHistory}>
+          <button
+            type="button"
+            className="btn_ty01 btn_search"
+            onClick={loadHistory}
+          >
             검색
           </button>
         </div>
@@ -67,25 +89,48 @@ export default function RankingSetting() {
               </tr>
             </thead>
             <tbody>
-              {resData?.length > 0 ?
-                resData?.map((el, idx) => {
+              {resData?.rankInfo.length > 0 ? (
+                resData?.rankInfo.map((el, idx) => {
                   return (
                     <tr key={idx}>
                       <td>{el.ranking}</td>
                       <td>{el.mb_id}</td>
                       <td>{el.mb_sex ? el.mb_sex : "-"}</td>
                       <td>{el.mb_birth ? el.mb_birth : "-"}</td>
-                      <td>{el.certify ? el.certify : "-"}</td>
-                      <td>{el.po_point ? el.po_point.toLocaleString("ko-KR") : 0}</td>
+                      <td>{el.mb_family ? el.mb_family : "-"}</td>
+                      <td>
+                        {el.point
+                          ? Number(el.point).toLocaleString("ko-KR")
+                          : 0}
+                      </td>
                     </tr>
                   );
-                }):<tr><td colSpan="6">데이터 없음</td></tr>}
+                })
+              ) : (
+                <tr>
+                  <td colSpan="6">데이터 없음</td>
+                </tr>
+              )}
             </tbody>
           </table>
         </div>
         {/* <CurrentBox add={false} mod={true} del={true} down={true} hideTit={true} /> */}
-        <CurrentBox btns={[/* "down" */]} hideTit={true} />
-        {pageData && <Pagination pageData={pageData} curPage={curPage} setCurPage={setCurPage} onClick={loadPageData} />}
+        <CurrentBox
+          btns={
+            [
+              /* "down" */
+            ]
+          }
+          hideTit={true}
+        />
+        {pageData && (
+          <Pagination
+            pageData={pageData}
+            curPage={curPage}
+            setCurPage={setCurPage}
+            onClick={loadPageData}
+          />
+        )}
         <Pagination />
       </div>
     </>
